15f88c30a52891dd76029131202ee25837da32dc
ZYP <596246199@qq.com>
2021-02-25 18:28:23 +0800

    更新树形无限下拉菜单

---
+++ TreeMenu/MenuItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="menu-item">
+      menu-item
+      <slot>
+      </slot>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"MenuItem"
+}
+</script>
+
+<style lang="stylus" scoped>
+.menu-item
+    height: 50px;
+    text-align: center;
+    line-height: 50px;
+</style>
---
+++ TreeMenu/ReSubMenu.vue
@@ -0,0 +1,25 @@
+<template>
+  <sub-menu bgColor='orange'
+    :style="'background-color:'+bgColor+';'">
+    <template #title>{{ data.title }}</template>
+    <template v-for="c of data.children">
+      <menu-item :key="c.id" v-if="!c.children">
+        {{ c.title }}
+      </menu-item>
+      <re-subMenu v-else :key="c.id" :data="c"></re-subMenu>
+    </template>
+  </sub-menu>
+</template>
+
+<script>
+export default {
+  name: "ReSubMenu",
+  props: {
+    data: Object,
+    bgColor: {
+      type: String,
+      default: "#000",
+    },
+  },
+};
+</script>
